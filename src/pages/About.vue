98fa0586288e78e1e83ffd2fc8c63f977bc9d56c
SatriaHilmi <vanpers34@gmail.com>
2025-08-02 14:59:35 +0700

    feat: learning vue navigation

---
+++ src/pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex p-4 text-center">
+        <h1 class="text-2xl font-bold">Welcome to DeskCore About</h1>
+        <router-link to="/" class="ml-4 text-blue-500 hover:underline">Go to Home</router-link>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex p-4 text-center">
+        <h1 class="text-2xl font-bold">Welcome to DeskCore home</h1>
+        <router-link to="/about" class="ml-4 text-blue-500 hover:underline">Go to About</router-link>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
